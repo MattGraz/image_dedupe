e4b238bf0bb64a5cf018fc58c6284f3dc87a9d94
Matt Graziano <matthew.graziano1@gmail.com>
2019-10-08 22:50:05 -0400

    reset

--- server.py
+++ server.py
@@ -1,60 +1,15 @@
-import subprocess
-import json
-import scipy
-import time
-import numpy as np
-from flask import Flask, Response
-
-BASE_DIR = "/home/its-an-avacado/plex/data/pictures"
-
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
-
-
-# @app.route("/echo", methods=['POST'])
-# def echo():
-#     command = request.form['text']
-#     subprocess.call([command])
-#     # return request.form['text'] + " Command executed via subprocess"
-#     return index()
-
-
-# @app.route("/calc", methods=['POST'])
-# def calc():
-#     subprocess.call('calc.exe')
-#     return index()
-
-
-# @app.route("/test", methods=['POST'])
-# def test():
-#     while True:
-#         subprocess.call('calc.exe')
-
-
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    return rv
-
-
-@app.route('/new_image')
-def generate_image():
-    def generate():
-        for i in range(10):
-            noise_image = np.random.uniform(
-                0, 256, (224, 224, 3)).astype('float32')
-            scipy.misc.imsave('out/' + str(i) + '.jpg', noise_image)
-            time.sleep(1)
-            yield(str(i) + '.jpg')
-
-    return Response(stream_template('index.html', data=generate()))
+@app.route('/')
+@app.route('/index')
+def show_index():
+    full_filename = '/home/its-an-avacado/repos/image_dedupe/out/10.jpg'
+    return render_template("index.html", user_image=full_filename)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
